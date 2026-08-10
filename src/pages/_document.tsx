d577398f5c13c7da31271cf42d4ef3423e79e5d4
Rod2003 <rodrigo.daguila@gmail.com>
2026-08-09 20:33:16 -0700

    Fix favicon quality

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/icon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
